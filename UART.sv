f0942217898e90507bd52692935623670c359e50
Dale Walker <dalewalker@vt.edu>
2026-06-30 23:29:08 -0400

    first commit

---
+++ UART.sv
@@ -0,0 +1,7 @@
+/*
+Author: Dale Walker 
+Project: UART + FIFO
+Description: Leveraging System Verilog to design a configurable UART module capable of transmitting and receiving serial 
+	data while using FIFOs to decouple the UART
+
+*/
