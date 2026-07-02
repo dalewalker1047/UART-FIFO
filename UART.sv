2f3da75478426e072a295b7b61e854629a3c15ce
Dale Walker <dalewalker@vt.edu>
2026-07-01 22:21:22 -0400

    Design planning phase

--- UART.sv
+++ UART.sv
@@ -1,7 +1,25 @@
 /*
-Author: Dale Walker 
+Author: Dale Walker
+Date: 6/29/2026
 Project: UART + FIFO
-Description: Leveraging System Verilog to design a configurable UART module capable of transmitting and receiving serial 
-	data while using FIFOs to decouple the UART
 
+Description: Leveraging System Verilog to design a configurable UART module capable of transmitting and receiving serial 
+data while using FIFOs to decouple the UART
+
+Skills showcased: FSM, Sychronous FIFO design, Clock division and baud rate gen, hardware modules, testing
+
+Includes Documentation
 */
+
+//UART transmitter
+//UArt Receiver
+//Baud Rate Generator
+//FIFO
+//Top Level
+//TB
+
+//possible parameters 
+//DATA_WIDTH
+//FIFO_DEPTH
+//CLOCK_FREQ
+//BAUD_RATE
