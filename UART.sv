de4d5d99b7e21211c17bde99150d15c4f91c6aef
Dale Walker <dalewalker@vt.edu>
2026-07-03 16:40:53 -0400

    Module planning

--- UART.sv
+++ UART.sv
@@ -24,9 +24,39 @@ Includes Documentation
 //CLOCK_FREQ
 //BAUD_RATE
 
-module UART(input d,       // Data input
-              input 			clk,     // Clock signal
-              input 			rstn,    // Active-low synchronous reset
-              output reg	q);    
+module UART(
+	input rx,       		// Data input
+	input logic clk,		// Clock signal
+	input logic	tx,		//
+	inout logic dvsr,		//
+	output reg tx_full,	//
+	output reg rx_empty);//    
 
 endmodule
+
+module baud_generator(
+	input logic clk, reset,
+	input logic [10:0] dvsr,
+	output logic tick);
+	
+endmodule
+
+module receiver(
+	input logic clk, reset, rx, s_tick,
+	output logic rx_done,
+	output logic [7:0] dout);
+	
+endmodule
+
+module transmitter(
+	input logic clk, reset, tx_start, s_tick,
+	input logic [7:0] din,
+	output logic tx_done,
+	output logic tx);
+	
+endmodule
+
+module fifo();
+
+endmodule
+	
